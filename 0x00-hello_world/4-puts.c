629063a5dbc349a48f851efb1d6a433e993eab53
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-10 07:08:25 -0700

    alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stido.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (sucess)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
