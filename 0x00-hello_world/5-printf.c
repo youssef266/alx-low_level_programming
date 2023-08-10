2d081edb9559f9d4b36c39d170f263009378b09f
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-10 08:02:15 -0700

    alx

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - is the main function
+ *
+ * Description: a c program to print with printf function
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
