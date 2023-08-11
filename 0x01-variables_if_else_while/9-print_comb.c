322b5ba0a2c4e827299bafca1009d9160999b514
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-11 16:56:27 -0700

    alx

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
