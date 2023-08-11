637371f72ad0298c76285c1e857601cb361b7967
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-11 16:04:06 -0700

    alx

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Decription: program that prints all single digit num from 0 ,10
+ *
+ * Return: alwayes 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d\n", i);
+	return (0);
+}
