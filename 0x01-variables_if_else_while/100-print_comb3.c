18df02f8fa0aaa2c23d4af19be0ac93ab5eb2d89
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-12 08:31:23 -0700

    alx

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all possible combinations of two digits
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			if (!(a > b) || a == b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+return (0);
+}
