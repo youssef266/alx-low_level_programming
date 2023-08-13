40ac39a3ad5a201f55ea175c77872a6ec6909f72
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-12 19:23:34 -0700

    alx

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  * main - entery point
  *
  * Description: prent a qoute using write function
- *
- * Reaturn: 1 (error)
+ *	ssize_t write(int fd, const void *buf, size_t count);
+ * Return: 1 (error).
 */
 
 int main(void)
-{	char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
