b80ed47b562bfd200b3cb3ddc38329f0d2716325
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-14 18:03:32 -0700

    alx

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,9 +10,9 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
