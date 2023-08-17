3b6207799db9937988dbc4da80f87699f143c385
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-16 21:56:33 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * return: 0
+*/
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	do {
+		_putchar(i + 48);
+		i++;
+	} while (i >= 0 && i <= 9);
+		_putchar('\n');
+}
