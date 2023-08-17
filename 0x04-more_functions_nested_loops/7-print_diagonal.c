4b402a29e5224857d0afc4e111fd0d8e1d6c9b43
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-17 08:01:21 -0700

    alx

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,11 +14,14 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	for (a = 1; a <= n; a++)
+	else
 	{
-		for (b = 1; b <= a; b++)
-			_putchar(' ');
-		_putchar(92);
-		_putchar('\n');
+		for (a = 1; a <= n; a++)
+		{
+			for (b = 1; b <= a; b++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
 	}
 }
