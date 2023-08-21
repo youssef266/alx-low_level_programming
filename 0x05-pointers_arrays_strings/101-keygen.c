8922e753814059b8704cc588f18860d90b889dfe
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-21 10:49:06 -0700

    alx

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,47 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 8
-
 /**
- * generate_password - function that generate password
- *
- * @password: input param
- *
- * Return: 0
-*/
-
-void generate_password(char *password)
-{
-	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFG";
-	int charset_length = sizeof(charset) - 1;
-	int i;
-
-	for (i = 0; i < PASSWORD_LENGTH; i++)
-	{
-		int index = rand() % charset_length;
-
-		password[i] = charset[index];
-	}
-
-	password[PASSWORD_LENGTH] = '\0';
-}
-
-/**
- * main - entery point
+ * main - function that generate password
  *
  * Return: 0
 */
 
 int main(void)
 {
+	int a;
+	char b;
+
 	srand(time(NULL));
-
-	char password[PASSWORD_LENGTH + 1];
-
-	generate_password(password);
-
-	printf("Generated Password: %s\n", password);
-
+	while (a <= 2645)
+	{
+		b = rand() % 128;
+		a += b;
+		putchar(b);
+	}
+	putchar(2772 - a);
 	return (0);
 }
