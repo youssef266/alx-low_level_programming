59efc4b1c07795a1cbee854279599a80ba8998a6
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-23 01:49:03 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ * @n: input
+ *
+ * Return: 0
+*/
+
+void print_number(int n)
+{
+	int divisor, temp;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	divisor = 1;
+	temp = n;
+
+	while (temp > 9)
+	{
+		divisor *= 10;
+		temp /= 10;
+	}
+	do {
+		_putchar('0' + (n / divisor));
+		n %= divisor;
+		divisor /= 10;
+	} while (divisor != 0);
+}
