1dcaf5cee9bc20f2a4e73d599ee3489aa387a849
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-08-30 01:23:44 -0700

    alx

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - find square root
+ * @n: intger
+ * @val: square root
+ *
+ * Return: int
+*/
+
+int square(int n, int val);
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - find square root
+ * @n: int
+ * @val: square root
+ *
+ * Return: int
+*/
+
+int square(int n, int val)
+{
+	if (val * val == n)
+	{
+		return (val);
+	} else if (val * val < n)
+	{
+		return (square(n, val + 1));
+	} else
+		return (-1);
+}
