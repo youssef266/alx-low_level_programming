74ed24630de23c57e5682457d4d3752b22f2f2ca
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-09-01 08:02:51 -0700

    alx

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: input int
+ * @argv: input char
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
