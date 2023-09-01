1bd4ac45b338827bf1c221ca2b7785f9efd8f286
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-09-01 08:05:20 -0700

    alx

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: input int
+ * @argv: input char
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
+
