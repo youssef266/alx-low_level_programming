bac11f57b2a046c1021749e43dd62579c4d7da74
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-09-01 15:41:49 -0700

    alx

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,45 +10,29 @@
 int main(int argc, char *argv[])
 {
 	int cents = atoi(argv[1]);
-	int coins_25 = 0, coins_10 = 0, coins_5 = 0, coins_2 = 0, coins_1 = 0;
+	int coins = 0;
+	int coin_values[] = {25, 10, 5, 2, 1};
+	int i;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	while (cents > 0)
+	for (i = 0; i < 5; i++)
 	{
-		if (cents >= 25)
+		while (cents >= coin_values[i])
 		{
-			cents -= 25;
-			coins_25++;
-		} else if (cents >= 10)
-		{
-			cents -= 10;
-			coins_10++;
-		} else if (cents >= 5)
-		{
-			cents -= 5;
-			coins_5++;
-		} else if (cents >= 2)
-		{
-			cents -= 2;
-			coins_2++;
-		} else
-		{
-			cents -= 1;
-			coins_1++;
+			cents -= coin_values[i];
+			coins++;
 		}
 	}
-
-	printf("%d %d %d %d %d\n", coins_25, coins_10, coins_5, coins_2, coins_1);
+	printf("%d\n", coins);
 
 	return (0);
 }
