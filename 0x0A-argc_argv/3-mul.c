57c9d37018c5ea5761c5a380bd9fb32e2bcea4c3
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-09-01 08:21:18 -0700

    alx

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: input int
+ * @argv: input char
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
