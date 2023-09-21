1d4eac3abc8d0515c910e2bf8c14b1c7276f1c27
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-09-21 02:19:59 -0700

    alx

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (!h->str)
+		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
-		else
+		} else
+		{
 			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
-		s++;
+		count++;
 	}
-	return (s);
+	return (count);
 }
 
