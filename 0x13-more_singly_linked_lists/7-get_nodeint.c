0c92b645bb6973c37259f4d42391e164a6fa0c34
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-09-25 11:18:32 -0700

    alx

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,9 +19,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		temporary = temporary->next;
 		number++;
 	}
-	if (temporary)
-	{
-		return (temporary);
-	}
-
+	return (temporary ? temporary : NULL);
 }
