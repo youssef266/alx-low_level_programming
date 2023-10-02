6b2ca8506911fdaef6269f295d05e5b29bcba24c
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-10-02 02:16:00 -0700

    alx

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, w_in_f, l = 0;
+	int fd, w_in_f, l = 0;
 
-    if (filename == 0 || filename == NULL)
-        return (-1);
-
-    if (text_content != NULL)
-    {
-        for (l = 0; text_content[l];)
-            l++;
-    }
-    /* creat file that read and write */
-    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    w_in_f = write(fd, text_content, l);
-
-    if (fd == -1 || w_in_f == -1)
-        return (-1);
-
-    close(fd);
-
-    return (1);
+	if (filename == 0 || filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
+	/* creat file that read and write */
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w_in_f = write(fd, text_content, l);
+	if (fd == -1 || w_in_f == -1)
+		return (-1);
+	close(fd);
+	return (1);
 }
