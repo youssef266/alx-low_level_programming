04c18ed46ff1eb9bbe460ace6e1c6820ff7c1052
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-01 13:40:19 +0200

    alx

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - a function that deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *header = ht;
+	hash_node_t *node_ht, *temporary;
+	unsigned long int num;
+
+	for (num = 0; num < ht->size; num++)
+	{
+		if (ht->array[num] != NULL)
+		{
+			node_ht = ht->array[num];
+			while (node_ht != NULL)
+			{
+				temporary = node_ht->next;
+				free(node_ht->key);
+				free(node_ht->value);
+				free(node_ht);
+				node_ht = temporary;
+			}
+		}
+	}
+	free(header->array);
+	free(header);
+}
